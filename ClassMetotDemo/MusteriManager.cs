52e83939c6d5e16b0ae6fff35f4cc72c126705a6
comlek <comlekahmetcan97@hotmail.com>
2022-01-19 22:32:56 +0300

    Program Hazır Sekli

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -6,5 +6,23 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.TcNo + " " + musteri.Sehir + " " + musteri.Adres + " " + musteri.Eposta + ": Belirtilen Müşteri Eklenmiştir.!\n");
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.TcNo + " " + musteri.Sehir + " " + musteri.Adres + " " + musteri.Eposta + ": Belirtilen Müşteri Silinmiştir.!\n");
+        }
+
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müsteriler Listelendi.!");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+            }
+        }
     }
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,28 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "AhmetCan";
+            musteri1.Soyad = "Comlek";
+            musteri1.TcNo = "12345678910";
+            musteri1.Sehir = "Manisa";
+            musteri1.Eposta = "ahmetcan@gmail.com";
+            musteri1.Adres = "Adres Ahmetcan";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Caner";
+            musteri2.Soyad = "Comlek";
+            musteri2.TcNo = "01234678910";
+            musteri2.Sehir = "İstanbul";
+            musteri2.Eposta = "caner@gmail.com";
+            musteri2.Adres = "Adres Caner";
+
+            MusteriManager musteriIslem = new MusteriManager();
+            musteriIslem.MusteriEkleme(musteri1);
+            musteriIslem.MusteriSilme(musteri2);
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            musteriIslem.MusteriListeleme(musteriler);
         }
     }
 }
